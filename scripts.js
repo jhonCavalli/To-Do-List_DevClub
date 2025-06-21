36985f9be4f04cb2816c5da6dcf8fdb5de2b2110
jhon Cavalli <jhoncavalli@hotmail.com>
2025-06-21 16:42:43 -0300

    ajusta espaçamento no HTML e melhora a legibilidade do código no scripts.js

--- scripts.js
+++ scripts.js
@@ -3,10 +3,16 @@ lista de tarefa
 [x] saber quando o botão foi clicado
 [] pega o texto do input
 [] colocar esse exto na tela
- [] deletar a tarefa
+[] deletar a tarefa na tela 
  */
 
 function adicionarTarefa(){
-   let input = document.querySelector("input").value
-   
+   let valorDoInput = document.querySelector("input").value
+
+   let li = document.createElement("li");
+   li.innerHTML = valorDoInput + "<span>X</span>";  
+
+   document.querySelector("ul").appendChild(li);
+   valorDoInput = ""; // Limpa o input após adicionar a tarefa
+    
 }
