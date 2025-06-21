98039d07a2126e996446285c339f169ca10f26a9
jhon Cavalli <jhoncavalli@hotmail.com>
2025-06-21 16:27:58 -0300

    adiciona funcionalidade para adicionar tarefa ao clicar no botão e implementa esboço da função no scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*
+lista de tarefa 
+[x] saber quando o botão foi clicado
+[] pega o texto do input
+[] colocar esse exto na tela
+ */
+
+function adicionarTarefa() {
+    console.log("Botão clicado");
+}
